8d8baac248f03852639722528cdea854d90307fc
shrithick <shrithick@Shrithick.pkrmbendc.com>
2022-11-15 16:30:16 +0530

    TouristManagementSystem

--- TouristManagement-2/src/main/java/com/example/demo/TouristManagement2Application.java
+++ TouristManagement-2/src/main/java/com/example/demo/TouristManagement2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TouristManagement1Application {
+public class TouristManagement2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TouristManagement1Application.class, args);
+		SpringApplication.run(TouristManagement2Application.class, args);
 	}
 
 }
--- TouristManagement-2/src/test/java/com/example/demo/TouristManagement2ApplicationTests.java
+++ TouristManagement-2/src/test/java/com/example/demo/TouristManagement2ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TouristManagement1ApplicationTests {
+class TouristManagement2ApplicationTests {
 
 	@Test
 	void contextLoads() {
